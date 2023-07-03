21afb52688d0b8ca59196ac2821d07773261ec01
Sergio Perea de la Casa <spc00033@red.ujaen.es>
2023-07-03 16:51:45 +0200

    Añadidos los métodos para obtener únicamente el contorno en los puntos de interés de cada máscara.

--- Mask R-CNN/Transformacion/creacion_json_dataset.py
+++ Mask R-CNN/Transformacion/creacion_json_dataset.py
@@ -4,7 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-def obtener_coordenadas_color_rango(ruta_imagen, rango_color, tolerancia=10):
+
+'''
+Método para obtener los puntos de interés tanto del contorno como del interior a partir de una reducción de almacenamiento
+de los píxeles de cada 10 píxeles.
+'''
+def obtener_coordenadas_color_tolerancia(ruta_imagen, rango_color, tolerancia=10):
     # Leer la imagen
     imagen = cv2.imdecode(np.fromfile(ruta_imagen, dtype=np.uint8), cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
 
@@ -35,6 +40,66 @@ def obtener_coordenadas_color_rango(ruta_imagen, rango_color, tolerancia=10):
     return coordenadas_x, coordenadas_y
 
 
+
+def obtener_coordenadas_color_contornos(imagen_path, color_objetivo):
+    # Leer la imagen
+    imagen = cv2.imdecode(np.fromfile(imagen_path, dtype=np.uint8), cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
+
+    # Convertir la imagen a escala de grises resaltando el color objetivo
+    imagen_grises_resaltada = convertir_a_escala_de_grises(imagen, color_objetivo)
+
+    # Encontrar los contornos de los objetos resaltados
+    contornos, _ = cv2.findContours(imagen_grises_resaltada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Obtener las coordenadas (x, y) de todos los puntos del contorno
+    coordenadas_x = []
+    coordenadas_y = []
+    for cnt in contornos:
+        for punto in cnt:
+            x, y = punto[0]
+            coordenadas_x.append(int(x))
+            coordenadas_y.append(int(y))
+
+    return coordenadas_x, coordenadas_y
+
+def convertir_a_escala_de_grises(imagen, color_objetivo):
+    # Convertir la imagen a formato BGR
+    imagen_bgr = cv2.cvtColor(imagen, cv2.COLOR_RGB2BGR)
+    
+    # Obtener la máscara del color objetivo
+    mascara = cv2.inRange(imagen_bgr, color_objetivo, color_objetivo)
+    
+    # Aplicar la máscara a la imagen original
+    imagen_resultante = cv2.bitwise_and(imagen_bgr, imagen_bgr, mask=mascara)
+    
+    # Convertir la imagen resultante a escala de grises
+    imagen_gris = cv2.cvtColor(imagen_resultante, cv2.COLOR_BGR2GRAY)
+    
+    return imagen_gris
+
+
+def mostrar_puntos_de_interes(imagen_path, coordenadas_x, coordenadas_y, color):
+
+    imagen = cv2.imdecode(np.fromfile(imagen_path, dtype=np.uint8), cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
+
+    # Crear una máscara vacía
+    mascara = np.zeros(imagen.shape[:2], dtype=np.uint8)
+    
+    # Dibujar los puntos de interés en la máscara
+    for x, y in zip(coordenadas_x, coordenadas_y):
+        cv2.circle(mascara, (x, y), 5, color, -1)
+    
+    # Aplicar la máscara a la imagen original
+    imagen_con_puntos = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+    imagen_con_puntos = cv2.bitwise_and(imagen_con_puntos, imagen_con_puntos, mask=mascara)
+    
+    # Mostrar la imagen resultante
+    plt.imshow(imagen_con_puntos)
+    plt.axis('off')
+    plt.show()
+
+
+
 def obtener_json_segmentacion(carpeta_imagenes, carpeta_jsons, color_segmentacion):
     # Lista para almacenar los datos de cada imagen
     datos_imagenes = []
@@ -50,7 +115,9 @@ def obtener_json_segmentacion(carpeta_imagenes, carpeta_jsons, color_segmentacio
             extension = os.path.splitext(filename)[1].lower()
             if extension == '.jpg' or extension == '.png' or extension == '.JPG':
                 # Obtener coordenadas de color en la imagen
-                coordenadas_x, coordenadas_y = obtener_coordenadas_color_rango(ruta_imagen, color_segmentacion)
+                coordenadas_x, coordenadas_y = obtener_coordenadas_color_tolerancia(ruta_imagen, color_segmentacion)
+                #coordenadas_x, coordenadas_y = obtener_coordenadas_color_contornos(ruta_imagen, color_segmentacion)
+                mostrar_puntos_de_interes(ruta_imagen,coordenadas_x,coordenadas_y,(255,0,0))
                 
                 # Verificar que se encontraron coordenadas
                 if coordenadas_x and coordenadas_y:
@@ -121,14 +188,14 @@ def obtener_json_segmentacion(carpeta_imagenes, carpeta_jsons, color_segmentacio
     
 
 # Carpeta que contiene las imágenes y subcarpetas
-carpeta_imagenes = r"Dataset_pruebas/Masks/val"
-carpeta_jsons = r"Dataset_pruebas/Images/val"
+carpeta_imagenes = r"C:\TFG\0_BBDD_ETIQUETADAS\Dataset_pruebas\Masks\train"
+carpeta_jsons = r"C:\TFG\0_BBDD_ETIQUETADAS\Dataset_pruebas\Images\train"
 
 # Rangos de color para buscar en las imágenes
 color_segmentacion = (37, 177, 90)
 
 # Especificar la ruta y el nombre de archivo donde se guardará el JSON
-archivo_json = r"Dataset_pruebas/Images/val/puntos_de_interes.json"
+archivo_json = r"C:\TFG\0_BBDD_ETIQUETADAS\Dataset_pruebas\Images\train\puntos_de_interes.json"
 
 # Obtener los datos de segmentación en formato JSON para todas las imágenes en la carpeta
 datos_imagenes = obtener_json_segmentacion(carpeta_imagenes, carpeta_jsons, color_segmentacion)
@@ -141,7 +208,7 @@ with open(archivo_json, "w") as archivo:
 import json
 
 # Especificar la ruta y el nombre de archivo donde se guardará el JSON
-archivo_json = r"Dataset_pruebas/Images/val/puntos_de_interes.json"
+archivo_json = r"C:\TFG\0_BBDD_ETIQUETADAS\Dataset_pruebas\Images\train\puntos_de_interes.json"
 
 print("PRIMER ARCHIVO COMPLETADO: ", str(archivo_json))
 
@@ -156,9 +223,9 @@ contenido_modificado = contenido_modificado.replace("ñ", "n").replace("Ñ","N")
 contenido_modificado = contenido_modificado.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u")
 contenido_modificado = contenido_modificado[1:-1]  # IMPORTANTE: PARA MANTENER LA MISMA ESTRUCTURA, HAY QUE ELIMINAR EL PRIMER Y ÚLTIMO CARACTER DEL ARCHIVO FIXEADO.
 
-# Guardar la cadena de texto modificada en el mismo archivo o en un nuevo archivo
 # Guardar la cadena de texto modificada en un nuevo archivo
-nuevo_archivo_json = r"Dataset_pruebas/Images/val/puntos_de_interes_nuevo.json"
+nuevo_archivo_json = r"C:\TFG\0_BBDD_ETIQUETADAS\Dataset_pruebas\Images\train\puntos_de_interes_nuevo.json"
+
 with open(nuevo_archivo_json, "w") as archivo:
     archivo.write(contenido_modificado)
 
