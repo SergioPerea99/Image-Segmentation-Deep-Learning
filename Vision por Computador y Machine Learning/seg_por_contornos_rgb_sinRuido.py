64558c5b3ece31e5c8f6553a83dea5f7e5379f7e
Sergio Perea de la Casa <spc00033@red.ujaen.es>
2023-08-26 14:10:32 +0200

    Clean code: Contornos activos VC

--- Vision por Computador y Machine Learning/seg_por_contornos_rgb_sinRuido.py
+++ Vision por Computador y Machine Learning/seg_por_contornos_rgb_sinRuido.py
@@ -2,55 +2,56 @@ import os
 import cv2
 import numpy as np
 
-# Definir la ruta a la carpeta de entrada y salida
-carpeta_entrada = 'C:\TFG\DATASET_COMPLETO_ETIQUETADO\\4_CONTORNOS\seg_contornos_180_255_THRESH_TOZERO_INV_INTERESANTE'
-carpeta_salida = 'C:\TFG\DATASET_COMPLETO_ETIQUETADO\\8_CONTORNOS_CONTORNOS_INVERSOS_sinRuido\contornos_180_255_THRESH_TOZERO_INV_INTERESANTE_60_200_THRESH_TOZERO'
+def cargar_imagen(ruta):
+    return cv2.imread(ruta)
 
-umbral_area = 60 #Píxeles de umbral de área para limpieza de los que quedan sueltos.
+def convertir_a_escala_grises(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Crear la carpeta de salida si no existe
-os.makedirs(carpeta_salida, exist_ok=True)
-cont = 0
+def aplicar_umbral(img_gris, umbral_min, umbral_max):
+    _, img_binaria = cv2.threshold(img_gris, umbral_min, umbral_max, cv2.THRESH_TOZERO)
+    return img_binaria
 
-# Enumerar todos los archivos en la carpeta de entrada
-for nombre_archivo in os.listdir(carpeta_entrada):
-    # Construir la ruta completa al archivo de entrada
-    ruta_entrada = os.path.join(carpeta_entrada, nombre_archivo)
-    
-    # Cargar la imagen en color
-    img = cv2.imread(ruta_entrada)
-    
-    # Convertir la imagen a escala de grises
-    img_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Aplicar un umbral para obtener una imagen binaria
-    _, img_binaria = cv2.threshold(img_gris, 60, 200, cv2.THRESH_TOZERO)
-    
-    # Encontrar los contornos en la imagen binaria
-    contornos, _ = cv2.findContours(img_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Filtrar los contornos para eliminar los que estén sueltos
+def filtrar_contornos(contornos, umbral_area):
     contornos_filtrados = []
     for contorno in contornos:
         area = cv2.contourArea(contorno)
-        if area > umbral_area:  # Establecer un umbral de área mínimo para considerar un contorno como válido
+        if area > umbral_area:
             contornos_filtrados.append(contorno)
+    return contornos_filtrados
 
-    # Crear una máscara en blanco del mismo tamaño que la imagen original
+def crear_mascara(img, contornos):
     mask = np.zeros_like(img)
+    cv2.drawContours(mask, contornos, -1, (255, 255, 255), thickness=cv2.FILLED)
+    return mask
 
-    # Dibujar los contornos filtrados en la máscara
-    cv2.drawContours(mask, contornos_filtrados, -1, (255, 255, 255), thickness=cv2.FILLED)
+def aplicar_mascara(img, mask):
+    return cv2.bitwise_and(img, mask)
 
-    
-    # Aplicar la máscara a la imagen original para obtener la imagen segmentada
-    img_segmentada = cv2.bitwise_and(img, mask)
-    
-    # Construir la ruta completa al archivo de salida
-    ruta_salida = os.path.join(carpeta_salida, nombre_archivo)
-    
-    # Guardar la imagen segmentada
-    cv2.imwrite(ruta_salida, img_segmentada)
-    
-    cont += 1
-    print("Imagen segmentada número " + str(cont))
+if __name__ == "__main__":
+    carpeta_entrada = r"C:\TFG\0_BBDD_COMPLETAS\BBDD_completa\Primer enlace\Lynx_pardinus\Babel"
+    carpeta_salida = r"C:\TFG\2_MODELOS_VC\DATASET_PRUEBAS_ETIQUETADO\4_CONTORNOS\contornos_180_255_THRESH_TOZERO_INV_INTERESANTE_60_200_THRESH_TOZERO"
+    umbral_area = 60
+
+    os.makedirs(carpeta_salida, exist_ok=True)
+    cont = 0
+
+    for nombre_archivo in os.listdir(carpeta_entrada):
+        ruta_entrada = os.path.join(carpeta_entrada, nombre_archivo)
+        img = cargar_imagen(ruta_entrada)
+        
+        img_gris = convertir_a_escala_grises(img)
+        img_binaria = aplicar_umbral(img_gris, 60, 200)
+        
+        contornos, _ = cv2.findContours(img_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contornos_filtrados = filtrar_contornos(contornos, umbral_area)
+        
+        mask = crear_mascara(img, contornos_filtrados)
+        
+        img_segmentada = aplicar_mascara(img, mask)
+        
+        ruta_salida = os.path.join(carpeta_salida, nombre_archivo)
+        cv2.imwrite(ruta_salida, img_segmentada)
+        
+        cont += 1
+        print("Imagen segmentada número " + str(cont))
